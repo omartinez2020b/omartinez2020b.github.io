01a28b8f98275667d53aa985d445746bc7bfeeaa
Oscar Martinez <o.martinez.2020@alumnos.urjc.es>
2021-12-16 17:20:36 +0100

    Added camera test

--- tests/jstests-01/test1.js
+++ tests/jstests-01/test1.js
@@ -22,8 +22,21 @@ window.addEventListener('DOMContentLoaded', function() {
 
         // Cylinder
         // <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+        let cylinder = document.createElement('a-cylinder');
+        cylinder.setAttribute('position', {x:1, y: 0.75, z: -3});
+        cylinder.setAttribute('radius', 0.5);
+        cylinder.setAttribute('height', 1.5);
+        cylinder.setAttribute('color', "#FFC65D");
+        scene.appendChild(cylinder);
 
         // Plane
         // <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+        let plane = document.createElement('a-plane');
+        plane.setAttribute('position', {x:0, y: 0, z: -4});
+        plane.setAttribute('rotation', {x:-90, y: 0, z: 0});
+        plane.setAttribute('width', 4);
+        plane.setAttribute('height', 4);
+        plane.setAttribute('color', "#7BC8A4");
+        scene.appendChild(plane);
     });
 });
